b4ef26e16b9e18555dbdbb1c124227cac25d1872
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2022-09-03 22:34:44 -0700

    Time: 119 ms (55.55%), Space: 43.1 MB (31.38%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,14 +3,17 @@
  * @param {number} target
  * @return {number[]}
  */
-let twoSum = (nums, target) => {
+const twoSum = (nums, target) => {
+    // map to store target sum
     const res = [0, 0], m = {};
     for(let i = 0; i < nums.length; i++){
+      // weve already looped through and found the number  
       if((target - nums[i]) in m){
          res[0] = m[target - nums[i]];
          res[1] = i;  
          return res; 
       } 
+      // add number to hashmap
       m[nums[i]] = i;
     }
     return res;
