55db7accb9eb70b6a441a2e8f375af24bdebed2e
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2022-09-04 14:45:09 -0700

    update longest substring

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -1,12 +1,16 @@
 const lengthOfLongestSubstring = function(s) {
+   // resultant variable 
    let max = 0;
+   // sliding window, JS set keeps order of insertion
    const c = new Set();
     
    for(let l = 0, r = 0; r < s.length;){
        if(!c.has(s[r])){
+        // set does not have the letter we've found a longer substring
            c.add(s[r++]);
            max = Math.max(c.size, max)
        } else {
+        // set has the letter delete the first letter and move the window
            c.delete(s[l++]);
        }
    }
--- 3-longest-substring-without-repeating-characters/NOTES.md
+++ 3-longest-substring-without-repeating-characters/NOTES.md
@@ -1,1 +1,6 @@
-​
+# Longest Substring Without Repeating Characters
+
+* Use a sliding window approach, store all letters in a set
+* If a letter is already present in the set increment the left pointer
+    * Remove it from the set as well
+* Repeat and update the maximum string length as you go
