7d3a08cb92f005dfa4a51ae1e66c28290010f512
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2022-09-04 18:18:10 -0700

    Update 429-n-ary-tree-level-order-traversal.js

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.js
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.js
@@ -12,16 +12,21 @@
  */
 const levelOrder = (root) => {
   if(!root) return [];
+  // initiate the BFS queue  
   let q = [root], res = [];  
   while(q.length !== 0) {
+     // create array to store this levels nodes
+     // and size to store this levels size 
      let level = [], size = q.length;
+     // iterate through this entire level 
      for(let i = 0; i < size; i++) {
          const front = q.shift();
-         for(let i of front.children) {
-            q.push(i); 
-         }
+         // push all n children onto queue
+         q.push(...front.children); 
+         // add to this current level
          level.push(front.val);
      } 
+     // push level onto result 
      res.push(level); 
   }
   return res;  
