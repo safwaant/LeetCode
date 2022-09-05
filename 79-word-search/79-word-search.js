d062be4f9b8526c25f8031be3daf5a3dbbc170a6
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2022-09-04 19:54:36 -0700

    Update 79-word-search.js

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -6,20 +6,26 @@
 let exist = (board, word) => { 
    const [h, w] = [board.length, board[0].length],
          findWordDFS = (r, c, i) => {
+            // succuessfully found all letters for the word 
             if(i === word.length) {
                 return true;
             } 
+            // bounds check and check to make sure the word is being made correctly 
             if(r < 0 || c < 0 || r === h || c === w || board[r][c] !== word[i]) {
                 return false;  
             } 
+            // keep track of letter to backtrack later 
             let temp = word[i++];
+            // mark visited 
             board[r][c] = "."; 
-            check = findWordDFS(r + 1, c, i) || findWordDFS(r - 1, c, i) || 
+            // run dfs on the board 
+            let check = findWordDFS(r + 1, c, i) || findWordDFS(r - 1, c, i) || 
                 findWordDFS(r, c + 1, i) || findWordDFS(r, c - 1, i);
+            // backtrack from an incorrect call to replace the board 
             board[r][c] = temp; 
             return check; 
          }; 
-    
+   // call dfs for every cell 
    for(let r = 0; r < h; r++) {
        for(let c = 0; c < w; c++) {
           if(findWordDFS(r, c, 0)) {
