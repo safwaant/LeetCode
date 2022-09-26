51b4d21d62e6edc622a6af7940c22ce5a0f765dd
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2022-09-26 15:50:12 -0700

    Update 219-contains-duplicate-ii.js

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.js
@@ -7,9 +7,11 @@ const containsNearbyDuplicate = (nums, k) => {
    let m = {};
    for(let i = 0; i < nums.length; i++) {
      const val = nums[i];
+     // check if the value is already in the map 
      if(val in m && Math.abs(i - m[val]) <= k) {
         return true; 
      } else {
+        // value isnt in the map add it to the map
         m[val] = i; 
      }  
    }
